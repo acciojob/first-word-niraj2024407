43819ef9bfacf0cd38e4b22215d776f1acfe292e
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-13 16:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	str = str.trim();
+
+	if(str === "") return"";
+
+	let spaceIndex = str.indexOf(" ");
+	
+	if (spaceIndex === -1) return str;
+	
+    return str.substring(0, spaceIndex);
+
+	
+	
 }
 
 // Do not change the code below
